2c75eeb1861369c7317d0fb4500e80198fcadf30
arizzen <a1.arizzen@gmail.com>
2024-05-07 13:07:36 +0100

    saS

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ const sr = ScrollReveal ({
 	reset: true
 })
 
-sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+
 
 sr.reveal('.sub-service,.about,.portfolio,.service,.cta,.contact',{delay:200, origin:'bottom'})
